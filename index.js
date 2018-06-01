38511682486f87a5edcbed5e5bb5b5609d3fec92
OligerMan <32018536+OligerMan@users.noreply.github.com>
2018-06-01 20:31:16 +0300

    New try

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var app = express();
 var http = require('http').Server(app);
 
 
-app.post('/alice-webhook', function(req, res, data){
-    console.log(data.version);
+app.post('/alice-webhook', function(req, res){
+    console.log(req.body.version);
     var ans = {
         version: '1.0',
-        session: data.session,
+        session: req.body.session,
         response: {
             text: 'kek',
             end_session: false
